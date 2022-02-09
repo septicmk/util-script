162b1a4d671c90e128e9b98b94bb910beb08b0a7
septicmk <septic.mk@gmail.com>
2022-02-09 09:21:28 +0800

    format bar

--- plot/bar.py
+++ plot/bar.py
@@ -5,17 +5,31 @@ from operator import add
 
 ycmap = ["#F7bc15", "#1a3ac5", "#3366ff", "#Abcbf6"]
 
+# draw single group bar
+# colors = ["k", "dimgray", "gray", "lightgray", "w", "r"]
+# labels = ["ORD+ORD", "ORD+ORDf", "ORDf+ORD", "ORDf+ORDf", "Alluxio"]
+# labels = colros
+def draw_barh0(ax, xname, data, labels, colors):
+    x = np.array(range(len(xname)))
+    rect=ax.barh(x, data, 0.6, color=colors, lw=1, edgecolor='k', label=labels)
+    ax.invert_xaxis()
+    ax.grid(ls='--')
+    ax.set_axisbelow(True)
+    #ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(True)
+    #ax.set_xscale('log')
+
 """ input:
 data = np.array([[2439.45, 164.634765],
                  [50.5063, 52.421669],
                  [56.4439, 14.584431]])
 """
-def draw_bar(ax, xname, data, labels, colors):
+def draw_bar(ax, xname, data, labels, colors, hatchs):
     w = 0.25;
     x = np.array(range(len(xname)))
-    ax.bar(x-w, data[0], w, label="IO", lw=1, edgecolor='k', color='k', hatch='+')
-    ax.bar(x, data[1], w, label="Serialization", edgecolor='k', color=ycmap[1])
-    ax.bar(x+w, data[2], w, label ="Deserialization", edgecolor='k', color='white', hatch='\\')
+    ax.bar(x-w, data[0], w, label=labels[0], lw=1, edgecolor='k',  color=colors[0], hatch=hatchs[0])
+    ax.bar(x,   data[1], w, label=labels[1], lw=1, edgecolor='k',  color=colors[1])
+    ax.bar(x+w, data[2], w, label =labels[2],lw=1, edgecolor='k',  color=colors[2], hatch=hatchs[2])
     ax.set_yscale('log')
     ax.set_ylabel('Runtime (s)')
     ax.set_xticks(x)
@@ -24,25 +38,27 @@ def draw_bar(ax, xname, data, labels, colors):
     #ax.set_axisbelow(True)
     #ax.legend(prop={'size': 6})
 
-def draw_bar(ax, xname, data, labels, colors):
-    mask = np.ones(15, dtype=bool)
-    mask[[2,4,6,8,9]] = False
-    opt = opt[mask]
-    dft = dft[mask]
-    print(opt)
-    width=0.35
-    #plt.get_cmap('gray')(np.linspace(0.15, 0.85, 3))
-    #ax.xaxis.set_visible(False)
-    #ax.yaxis.set_visible(False)
-    #x = np.arange(len(names))
-    rect=ax.bar(x-width/2, dft, width, color=cmaps[0], lw=1, edgecolor='k', label=lbl[0], hatch='+')
-    rect=ax.bar(x+width/2, opt, width, color=cmaps[1], lw=1, edgecolor='k', label=lbl[1], hatch='//')
+def draw_stack_bar(ax, xname, data, labels, colors):
+    #lbl = ["comp", "overhead"]
+    #comp     =   np.array([53, 53, 53])
+    #overhead = np.array([0, 23, 50.4])
+    #cmaps = [ycmap[1], ycmap[0]]
+    #x = np.array([0.2, 1, 1.8])
+    x = np.array(range(len(xname)))
+    #xname = ["co-location", "replication", "evivtion"]
+    width = 0.35
+    ax.bar(x, comp, width, color=cmaps[0], lw=1, edgecolor='k', label = lbl[0])
+    ax.bar(x, overhead, width, bottom=comp, color=cmaps[1], lw=1, edgecolor='k', label = lbl[1])
+
+    #_ax.get_xaxis().set_visible(False)
+    ax.set_ylabel('Runtime (s)')
     ax.set_xticks(x)
-    ax.set_xticklabels(names)
-    ax.grid(axis='y', ls='--')
-    #ax.set_yscale('log')
-    #ax.set_xlim(0, np.sum(data, axis=1).max())
-    #ax.set_ylim(-0.4, 7.4)
+    ax.set_xticklabels(xname)
+    ax.grid(ls='--')
+    ax.set_axisbelow(True)
+    ax.set_ylim(0,110)
+    ax.legend(prop={'size': 10})
+    # ax.set_yscale('log')
 
 def draw_stack_bar(_ax, ax, data):
     #base =   np.array(
@@ -87,38 +103,24 @@ def draw_stack_bar(_ax, ax, data):
     _ax.legend(prop={'size': 6})
     # ax.set_yscale('log')
 
-def draw_stack_bar(ax, xname, data, labels, colors):
-    #lbl = ["comp", "overhead"]
-    #comp     =   np.array([53, 53, 53])
-    #overhead = np.array([0, 23, 50.4])
-    #cmaps = [ycmap[1], ycmap[0]]
-    #x = np.array([0.2, 1, 1.8])
-    x = np.array(range(len(xname)))
-    #xname = ["co-location", "replication", "evivtion"]
-    width = 0.35
-    ax.bar(x, comp, width, color=cmaps[0], lw=1, edgecolor='k', label = lbl[0])
-    ax.bar(x, overhead, width, bottom=comp, color=cmaps[1], lw=1, edgecolor='k', label = lbl[1])
-
-    #_ax.get_xaxis().set_visible(False)
-    ax.set_ylabel('Runtime (s)')
-    ax.set_xticks(x)
-    ax.set_xticklabels(xname)
-    ax.grid(ls='--')
-    ax.set_axisbelow(True)
-    ax.set_ylim(0,110)
-    ax.legend(prop={'size': 10})
-    # ax.set_yscale('log')
-
-# draw single group bar
-# colors = ["k", "dimgray", "gray", "lightgray", "w", "r"]
-# labels = ["ORD+ORD", "ORD+ORDf", "ORDf+ORD", "ORDf+ORDf", "Alluxio"]
-# labels = colros
-def draw_barh0(ax, xname, data, labels, colors):
-    x = np.array(range(len(xname)))
-    rect=ax.barh(x, data, 0.6, color=colors, lw=1, edgecolor='k', label=labels)
-    ax.invert_xaxis()
-    ax.grid(ls='--')
-    ax.set_axisbelow(True)
+def draw_bar(ax, xname, data, labels, colors):
+    mask = np.ones(15, dtype=bool)
+    mask[[2,4,6,8,9]] = False
+    opt = opt[mask]
+    dft = dft[mask]
+    print(opt)
+    width=0.35
+    #plt.get_cmap('gray')(np.linspace(0.15, 0.85, 3))
     #ax.xaxis.set_visible(False)
-    ax.yaxis.set_visible(True)
-    #ax.set_xscale('log')
+    #ax.yaxis.set_visible(False)
+    #x = np.arange(len(names))
+    rect=ax.bar(x-width/2, dft, width, color=cmaps[0], lw=1, edgecolor='k', label=lbl[0], hatch='+')
+    rect=ax.bar(x+width/2, opt, width, color=cmaps[1], lw=1, edgecolor='k', label=lbl[1], hatch='//')
+    ax.set_xticks(x)
+    ax.set_xticklabels(names)
+    ax.grid(axis='y', ls='--')
+    #ax.set_yscale('log')
+    #ax.set_xlim(0, np.sum(data, axis=1).max())
+    #ax.set_ylim(-0.4, 7.4)
+
+

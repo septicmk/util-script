d8f641e506d2ca8a2a1bc42d37d461d3073af1d9
septicmk <septic.mk@gmail.com>
2022-02-14 17:02:57 +0800

    update heatmap

--- plot/heatmap.py
+++ plot/heatmap.py
@@ -3,11 +3,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 import sys
 
-def draw_heatmap(ax, data):
-    #im = ax.imshow(data, cmap='gray')
-    #im = ax.imshow(data, cmap='gist_heat')
-    im = ax.imshow(data, cmap='bone')
-    ax.set_title(name,fontsize=10)
-    ax.set_xticks(np.arange(0,20,5))
-    ax.set_yticks(np.arange(8))
+def draw_heatmap(ax, data, cmaps, name):
+    im = ax.imshow(data, cmap=cmaps) # gray gist_heat
+    ax.set_title(name, fontsize=10)
+    #ax.set_xticks(np.arange(0,20,5))
+    #ax.set_yticks(np.arange(8))
     #ax.set_xticklabels(sGPU)
